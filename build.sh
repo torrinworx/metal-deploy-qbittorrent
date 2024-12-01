059f67a657cc5329565123ef8d4f8e7f02adcf3b
torrin1 <torrin@worx4you.com>
2024-12-01 17:28:20 -0500

    Fixing vue root webui path

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 set -e
-
 apt update
 apt install -y qbittorrent-nox unzip wget
 wget https://github.com/VueTorrent/VueTorrent/releases/download/v2.18.0/vuetorrent.zip
 
 mkdir -p /home/metal_deploy_qbittorrent/.config/qBittorrent
-
 mkdir -p ./build/vuetorrent
 unzip vuetorrent.zip -d ./build/vuetorrent
 rm vuetorrent.zip
@@ -15,10 +13,15 @@ rm vuetorrent.zip
 # Path to the qBittorrent config
 CONFIG_FILE="/home/metal_deploy_qbittorrent/.config/qBittorrent/qBittorrent.conf"
 
-# Pre-set the PBKDF2 password hash
-if ! grep -q "WebUI\\Password_PBKDF2=" "$CONFIG_FILE"; then
+# Configure qBittorrent with Web UI settings
+if ! grep -q "WebUI\\AlternativeUIEnabled=" "$CONFIG_FILE"; then
     echo '[Preferences]' > "$CONFIG_FILE"
     echo 'WebUI\Password_PBKDF2=@ByteArray(ARQ77eY1NUZaQsuDHbIMCA==:0WMRkYTUWVT9wVvdDtHAjU9b3b7uB8NR1Gur2hmQCvCDpm39Q+PsJRJPaCU51dEiz+dTzh8qbPsL8WkFljQYFQ==)' >> "$CONFIG_FILE"
+    echo 'WebUI\AlternativeUIEnabled=true' >> "$CONFIG_FILE"
+    echo "WebUI\RootFolder=/home/metal_deploy_qbittorrent/build/vuetorrent" >> "$CONFIG_FILE"
+else
+    sed -i 's|^WebUI\\AlternativeUIEnabled=.*|WebUI\\AlternativeUIEnabled=true|' "$CONFIG_FILE"
+    sed -i "s|^WebUI\\RootFolder=.*|WebUI\\RootFolder=/home/metal_deploy_qbittorrent/build/vuetorrent|" "$CONFIG_FILE"
 fi
 
 cat <<'EOF' > ./build/run.sh
