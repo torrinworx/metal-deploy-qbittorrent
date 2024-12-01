a1733af84118ed849ff3267558b8420ce2a9ca4e
torrin1 <torrin@worx4you.com>
2024-12-01 15:38:12 -0500

    Fix

--- build.sh
+++ build.sh
@@ -10,18 +10,10 @@ mkdir -p ./build/vuetorrent
 unzip vuetorrent.zip -d ./build/vuetorrent
 rm vuetorrent.zip
 
-CONFIG_DIR="/home/metal_deploy_qbittorrent/.config/qBittorrent"
-mkdir -p "$CONFIG_DIR/qBittorrent/cache"
-chown -R metal_deploy_qbittorrent: "$CONFIG_DIR"
-chmod -R 755 "$CONFIG_DIR"
-
 cat <<'EOF' > ./build/run.sh
 #!/bin/bash
 
-CONFIG_DIR="/home/metal_deploy_qbittorrent/.config/qBittorrent"
-mkdir -p "$CONFIG_DIR/qBittorrent/cache"
-
-exec qbittorrent-nox --webui-root=./vuetorrent
+exec yes y | qbittorrent-nox
 EOF
 
 chmod +x ./build/run.sh
