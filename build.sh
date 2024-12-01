d27178f2a90e380a8efd944df68e1446a3fdac68
torrin1 <torrin@worx4you.com>
2024-12-01 15:06:01 -0500

    fix

--- build.sh
+++ build.sh
@@ -1,34 +1,21 @@
 #!/bin/bash
 
 set -e
+
 apt update
-apt install -y build-essential pkg-config libboost-system-dev \
-	libboost-chrono-dev libboost-random-dev libssl-dev \
-	qt6-base-dev qt6-tools-dev qt6-tools-dev-tools qt6-svg-dev \
-	zlib1g-dev python3 libtorrent-rasterbar-dev cmake
+apt install -y qbittorrent-nox unzip wget
+wget https://github.com/VueTorrent/VueTorrent/releases/download/v2.18.0/vuetorrent.zip
 
-export CMAKE_PREFIX_PATH=/usr/lib/x86_64-linux-gnu/cmake/Qt6
+mkdir -p ./build/vuetorrent
+unzip vuetorrent.zip -d ./build/vuetorrent
+rm vuetorrent.zip
 
-rm -rf qBittorrent
-rm -rf qBittorrent-release-5.0.2
-
-wget https://github.com/qbittorrent/qBittorrent/archive/refs/tags/release-5.0.2.tar.gz -O ./qBittorrent.tar.gz
-tar -xvzf ./qBittorrent.tar.gz -C ./
-mv qBittorrent-release-5.0.2 qBittorrent
-
-mkdir -p qBittorrent/build
-cd qBittorrent
-cmake -B build -DCMAKE_BUILD_TYPE=Release
-cmake --build build
-
-cd ..
-mkdir -p ./build/
 cat <<'EOF' > ./build/run.sh
 #!/bin/bash
 
-exec "./qBittorrent/build/src/app/qbittorrent"
+exec qbittorrent-nox --webui-root=./vuetorrent
 EOF
 
 chmod +x ./build/run.sh
-rm ./qBittorrent.tar.gz
-echo "Build complete. Run './build/run.sh' to start qBittorrent."
+
+echo "Build complete. Run './build/run.sh' to start qBittorrent with VueTorrent UI."
