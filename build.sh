a6797ece01558f8b413383e865ac3c2a85642461
torrin1 <torrin@worx4you.com>
2024-12-01 12:49:44 -0500

    Fixing build version

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 set -e
-
-# Update system and install necessary dependencies
 apt update
 apt install -y curl qttools5-dev-tools build-essential pkg-config automake libtool libssl-dev libboost-dev libboost-system-dev libboost-chrono-dev libboost-random-dev qtbase5-dev qt5-qmake zlib1g-dev
 
-# Function to determine architecture
 arch=$(dpkg --print-architecture)
 case $arch in
 	amd64) arch="x86_64" ;;
@@ -15,23 +12,19 @@ case $arch in
 	*) echo "Unsupported architecture: $arch" && exit 1 ;;
 esac
 
-# Download and extract qBittorrent source code
 wget "https://github.com/qbittorrent/qBittorrent/archive/refs/tags/release-5.0.2.tar.gz" -O ./qBittorrent.tar.gz
 tar -xvzf ./qBittorrent.tar.gz -C ./
-cd qBittorrent-release-4.5.0
+cd qBittorrent-release-5.0.2
 
-# Build qBittorrent
 ./configure --prefix=$HOME/.local
 make
 make install
 
-# Setup directories
 cd ..
 mkdir -p ./build/
-mv ./qBittorrent-release-4.5.0 ./build/qbittorrent/
+mv ./qBittorrent-release-5.0.2 ./build/qbittorrent/
 mkdir -p ~/.qbittorrent-data
 
-# Create a run script
 cat <<'EOF' > ./build/run.sh
 #!/bin/bash
 
